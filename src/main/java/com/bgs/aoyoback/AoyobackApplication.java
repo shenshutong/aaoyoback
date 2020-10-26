970279a32527d17ee418999abcb4c004a725eee2
shenshutong <13780892097@163.com>
2020-10-26 19:14:28 +0800

    Initial commit

---
+++ src/main/java/com/bgs/aoyoback/AoyobackApplication.java
@@ -0,0 +1,15 @@
+package com.bgs.aoyoback;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.bgs.aoyoback.mapper")
+public class AoyobackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AoyobackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bgs/aoyoback/AoyobackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgs.aoyoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AoyobackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
